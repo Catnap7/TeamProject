a085fc1aceffffa50031b670373fb7c907a80421
CHLEE931125 <38392147+CHLEE931125@users.noreply.github.com>
2018-06-04 17:42:18 +0900

    dddd

--- TeamProject0503/src/net/member/db/MemberDAO.java
+++ TeamProject0503/src/net/member/db/MemberDAO.java
@@ -242,7 +242,7 @@ private Connection getConnection() throws Exception {
 			rs = pstmt.executeQuery();
 
 			while(rs.next()) {
-						if(rs.getInt("m_grade")==1 ||rs.getInt("m_grade")==2) {
+						if(rs.getInt("m_grade")==1 || rs.getInt("m_grade")==2) {
 							Echeck=1;
 						}else {
 							Echeck=0;
