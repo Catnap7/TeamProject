f7ab191b2fc697fdbd7155b820809d0df1fd0f79
CHLEE931125 <38392147+CHLEE931125@users.noreply.github.com>
2018-06-05 19:03:52 +0900

    dddd

--- TeamProject0503/src/net/admin/manage/db/AdminSuspendDAO.java
+++ TeamProject0503/src/net/admin/manage/db/AdminSuspendDAO.java
@@ -69,5 +69,34 @@ public class AdminSuspendDAO {
 		}
 		return grade;
 	}
+	
+	public int AdminReviewReport(int r_p_num, String id) {
+		Connection con = null;
+		String sql = "";
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		int reportCheck = 0;
+		System.out.println(r_p_num);
+		System.out.println(id);
+		try {
+			con = getConnection();
+			sql = "select r_report from review where r_id = ? and r_p_num = ?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			pstmt.setInt(2, r_p_num);
+			rs = pstmt.executeQuery();			
+			if(rs.next()) {
+				reportCheck = rs.getInt("r_report");
+			}			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(rs != null)try{rs.close();}catch(SQLException ex){ex.printStackTrace();}
+			if(pstmt != null)try{pstmt.close();}catch(SQLException ex){ex.printStackTrace();}
+			if(con != null)try{con.close();}catch(SQLException ex){ex.printStackTrace();}				
+		}
+		System.out.println(reportCheck);
+		return reportCheck;
+	}
 
 }
