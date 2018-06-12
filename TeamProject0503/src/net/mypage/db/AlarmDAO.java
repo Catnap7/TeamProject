f4173b8e90b9ab8338ddbaeb895b17d94afc6e7e
itwill <itwill@itwillbs02-5>
2018-06-12 16:45:13 +0900

    알람에 바로가기

--- TeamProject0503/src/net/mypage/db/AlarmDAO.java
+++ TeamProject0503/src/net/mypage/db/AlarmDAO.java
@@ -6,11 +6,15 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 import java.sql.DriverManager;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
+
+import net.admin.manage.db.MovieBean;
+import net.favorite.db.FavoriteBean;
 
 public class AlarmDAO {
 	//디비연결 메서드
@@ -173,6 +177,69 @@ public class AlarmDAO {
 					}
 		 return alarmlist;
 	 }//end list
+	
+	public Vector getAlarmlist(String id,int startRow, int pageSize){
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		PreparedStatement pstmt2=null;
+		ResultSet rs2=null;
+		String sql="";
+		Vector vector=new Vector();
+		List alarmList=new ArrayList();
+		List movieList=new ArrayList();
+		try {
+			//1,2 디비연결
+			con=getConnection();
+			//3 sql id에 해당하는 장바구니 정보 가져오기
+			sql="select * from alarm where a_id=? order by a_num desc limit ?,?";
+			pstmt=con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			pstmt.setInt(2, startRow-1);
+			pstmt.setInt(3, pageSize);
+			//4 rs 실행 저장
+			rs=pstmt.executeQuery();
+			//5 rs 데이터 있으면 장바구니 자바빈 객체 생성
+			//  rs => 자바빈 저장 =>basketList 배열한칸 저장
+			//  rs => b_g_num 
+			//  3 sql b_g_num 해당하는 상품정보가져오기
+			//  4 rs2  pstmt2 실행저장   
+			//  5 rs2데이터 있으면 상품 자바빈 객체 생성
+			//  rs2=>자바빈 저장 => goodsList한칸 저장
+			while(rs.next()){
+				AlarmBean ab=new AlarmBean();
+				ab.setA_id(rs.getString("a_id"));
+				ab.setA_num(rs.getInt("a_num"));					
+				ab.setA_end_day(rs.getString("a_end_day"));
+				ab.setA_start_day(rs.getString("a_start_day"));
+				ab.setA_alarm_name(rs.getInt("a_alarm_name"));
+				ab.setA_movie_name(rs.getString("a_movie_name"));
+				ab.setA_check(rs.getInt("a_check"));
+				//fb.setF_date(rs.getTimestamp("f_date"));
+				alarmList.add(ab);								
+				sql="select * from movie where mv_kor_title=?";
+				pstmt2=con.prepareStatement(sql);
+				pstmt2.setString(1, ab.getA_movie_name());
+				rs2=pstmt2.executeQuery();
+				if(rs2.next()){
+					MovieBean mb=new MovieBean();
+					mb.setMv_num(rs2.getInt("mv_num"));					
+					movieList.add(mb);
+				}
+			}
+			// vector 첫번째 칸 basketList 저장
+			// vector 두번째 칸 goodsList 저장
+			vector.add(alarmList);
+			vector.add(movieList);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally{
+			if(rs!=null)try{rs.close();}catch(SQLException ex){}
+			if(pstmt!=null)try{pstmt.close();}catch(SQLException ex){}
+			if(con!=null)try{con.close();}catch(SQLException ex){}
+		}
+		return vector;
+	}
 	
 	public List<AlarmBean> getAlarms(String id){
 		 List<AlarmBean> alarmlist = new ArrayList<AlarmBean>();
