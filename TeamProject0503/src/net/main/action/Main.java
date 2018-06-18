9803b328bda41588ada2d9055738f97aa8fa1f71
ITWILL <ITWILL@itwillbs02-26>
2018-06-18 11:41:19 +0900

    주석정리

--- TeamProject0503/src/net/main/action/Main.java
+++ TeamProject0503/src/net/main/action/Main.java
@@ -1,16 +1,12 @@
 package net.main.action;
 
-import java.util.Calendar;
 import java.util.List;
-import java.util.Timer;
-import java.util.Vector;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import net.admin.manage.db.MovieBean;
-import net.favorite.db.FavoriteBean;
 import net.favorite.db.FavoriteDAO;
 import net.follow.db.FollowDAO;
 import net.main.db.MainDAO;
@@ -21,7 +17,6 @@ public class Main implements Action{
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		System.out.println("Main execute");
 		HttpSession session=request.getSession();
-		
 		
 		
 		String id = (String)session.getAttribute("m_id");
@@ -72,9 +67,6 @@ public class Main implements Action{
 		
 		request.setAttribute("allreviewcount", allreviewcount);
 		
-		
-		
-		
 		request.setAttribute("favoritelist", favoritelist);
 		request.setAttribute("favoritecount", favoritecount);
 		request.setAttribute("Mostcount", Mostcount);
@@ -82,33 +74,6 @@ public class Main implements Action{
 		request.setAttribute("Bestmovie", Bestmovie);
 		request.setAttribute("adminSelectMovieList", adminSelectMovieList);
 		
-//		留ㅼ씪 �옄�젙留덈떎 由ъ뀑 �떎�뻾�떆�궎�뒗 硫붿냼�뱶
-//		System.out.println("由ъ뀑 硫붿냼�뱶 �떎�뿕 �떆�옉");
-//		DailyReset dailyReset = new DailyReset();
-//		Timer timer = new Timer();
-//		Calendar date = Calendar.getInstance();		
-//		date.set(Calendar.AM_PM,Calendar.AM);
-//		date.set(Calendar.HOUR,12);
-//		date.set(Calendar.MINUTE, 0);
-//		date.set(Calendar.SECOND, 0);
-//		date.set(Calendar.MILLISECOND, 0);
-		
-//		timer.scheduleAtFixedRate(dailyReset, date.getTime(), 1000*60*60*24); //1000諛�由ъ큹*60珥�*60遺�*24�떆媛�
-//		留ㅼ씪 �옄�젙留덈떎 由ъ뀑 �떎�뻾�떆�궎�뒗 硫붿냼�뱶
-		
-////		�떎�떆媛� �븣�엺 硫붿냼�뱶
-//		System.out.println("�떎�떆媛� �븣�엺 �떎�뿕 �떆�옉");
-//		RealtimeAlarm realtime = new RealtimeAlarm();
-//		Timer timer2 = new Timer();
-//		Calendar date2 = Calendar.getInstance();		
-//		date2.set(Calendar.AM_PM,Calendar.AM);
-//		date2.set(Calendar.HOUR,12);
-//		date2.set(Calendar.MINUTE, 0);
-//		date2.set(Calendar.SECOND, 0);
-//		date2.set(Calendar.MILLISECOND, 0);
-//		
-//		timer2.scheduleAtFixedRate(realtime, date2.getTime(), 1000*60); //1000諛�由ъ큹*60珥�*60遺�*24�떆媛�
-////		�떎�떆媛� �븣�엺 硫붿냼�뱶
 		
 		forward=new ActionForward();
 		forward.setRedirect(false);
