3e286942d7c79573e005f11d2263d59899237215
임채빈 <@LAPTOP-8B2T8EVL>
2018-06-19 15:37:59 +0900

    수정

--- TeamProject0503/src/net/vip/action/VipResDelete.java
+++ TeamProject0503/src/net/vip/action/VipResDelete.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import net.main.action.ActionForward;
 import net.vip.db.VipResBean;
 import net.vip.db.VipResDAO;
 
