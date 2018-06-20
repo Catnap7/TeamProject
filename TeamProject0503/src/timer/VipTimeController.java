3635eaff8e61e5c1d213eddb77b1c36f199e351a
ahn <dbwjd2575@gmail.com>
2018-06-20 14:27:50 +0900

    timer 날짜변경

--- TeamProject0503/src/timer/VipTimeController.java
+++ TeamProject0503/src/timer/VipTimeController.java
@@ -18,7 +18,7 @@ public class VipTimeController extends HttpServlet {
 		 
 		  date.set(Calendar.YEAR, 2018);
 		  date.set(Calendar.MONTH, 6);
-		  date.set(Calendar.DATE, 20);
+		  date.set(Calendar.DATE, 21);
 		  date.set(Calendar.AM_PM, Calendar.AM);
 		  date.set(Calendar.HOUR, 0);
 		  date.set(Calendar.MINUTE, 0);
