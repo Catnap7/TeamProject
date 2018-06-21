a30441052d1d6bf980fa79177e4f13ef32e14be8
ahn <dbwjd2575@gmail.com>
2018-06-21 15:44:37 +0900

    timer 시간 수정

--- TeamProject0503/src/timer/VipTimeController.java
+++ TeamProject0503/src/timer/VipTimeController.java
@@ -14,24 +14,28 @@ import net.vip.db.VipDAO;
 public class VipTimeController extends HttpServlet {
 	
 	public void init() throws ServletException{
-		Calendar date = Calendar.getInstance();
+
+		 Calendar date = Calendar.getInstance();
 		 
-		  date.set(Calendar.YEAR, 2018);
-		  date.set(Calendar.MONTH, 5);
-		  date.set(Calendar.DATE, 22);
+		//서버 시작한 후, 다음날 00시에 시작.
+		 date.getTimeInMillis();
+		 date.set(Calendar.DAY_OF_YEAR, date.get(Calendar.DAY_OF_YEAR)+1);
+
+		//오류날 경우, 날짜를 명시적으로 정해줄것. month는 0월부터 시작.
+//		  date.set(Calendar.YEAR, 2018);
+//		  date.set(Calendar.MONTH, 6);
+//		  date.set(Calendar.DATE, 21);
 		  date.set(Calendar.AM_PM, Calendar.AM);
 		  date.set(Calendar.HOUR, 0);
 		  date.set(Calendar.MINUTE, 0);
 		  date.set(Calendar.SECOND, 0);
 		  date.set(Calendar.MILLISECOND, 0);
-		  
+		
 		Timer timer = new Timer();
 		timer.scheduleAtFixedRate(new TimerTask() {
            public void run() {
                try {
-               	//런할때 실행시킬메소드. 
-            	
-            	//한달에 한번 vip선정   
+            	//4주에 한번 vip선정   
    				//먼저 vip인 회원의 그레이드를 낮추고,(메소드 실행)
    				VipDAO vdao= new VipDAO();
    				vdao.AutodeleteVip();
